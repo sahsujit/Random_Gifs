6994a1ec0279fafcb0e9b0009650f83df5471335
꧁༺քʀօɢʀǟʍʍɛʀ ֆʊʝɨȶ༻꧂ <129708170+sahsujit@users.noreply.github.com>
2023-12-09 18:05:49 +0545

    Update Tag.js

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -29,7 +29,7 @@ const Tag = () => {
 
   return (
     <div className="w-1/2 mb-[30px] bg-blue-500 mx-auto rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
-      <h1 className="text-3xl uppercase underline font-bold">Random {tag} Gif</h1>
+      <h1 className="text-3xl uppercase mt-[15px] underline font-bold">Random {tag} Gif</h1>
       {
         loading ? (<Spinner />) : (<img src= {gif} width="450" />)
       }
